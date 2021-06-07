a48425d3431eb5aa559c3b757fffda6c1dae0324
rajan <rajanmodi30@gmail.com>
2021-06-07 21:09:40 +0530

    -

--- src/views/Event/register.vue
+++ src/views/Event/register.vue
@@ -1,9 +1,17 @@
 <template>
   <div>Register Here</div>
+  <button @click="register">Click To Register</button>
 </template>
 
 <script>
 export default {
   name: "registerEvent",
+  methods: {
+    register() {
+      this.$router.push({
+        name: "details",
+      });
+    },
+  },
 };
 </script>
